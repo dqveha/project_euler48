3a0b07c3af3566beb4217dcae00f754b31783ba3
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-09-01 10:39:48 -0700

    Initial commit
    Co-authored-by: Kristen Hopper <hopperdavis@gmail.com>

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import $ from 'jquery'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './css/styles.css'
+
+$("section").text("TEST TEXT INPUT")
+
+
+
+
+// The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+//Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
